5eaf683a9bb30612d46d4bec36ed24ebf5a9d400
= <=>
2022-03-15 20:19:03 -0300

    [WIP] create project structure and started UtmForm component

---
+++ components/UtmForm.tsx
@@ -0,0 +1,16 @@
+
+export function UtmForm() {
+  return (
+    <form>
+      <label>
+        <span>URL</span>
+        <input type="text" name="url" />
+      </label>
+      <label>
+        <span>Campaign</span>
+        <input type="text" name="utm_campaign" />
+      </label>
+      
+    </form>
+  )
+}
